97b572701c1ce22235a49ed6be797405b00bf71e
AminaAbdallah <58793357+AminaAbdallah@users.noreply.github.com>
2020-02-10 12:15:44 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,23 @@
 
 
 
+
 from CurrencyAPI.Fetch_historical_data import Fetch_historical_data
 from CurrencyAPI.currency_conversion import Currency_conversion
 from CurrencyAPI.Fetch_data_in_aSpecific_timeFrame_conversion import Fetch_data_InSpecific_TimeFrame
+import os
+
+
+access_key = os.environ.get('b36407a99f24a168e534d190404b18f7')
 
 
 
+Fetch_historical_data('access_key','2018-02-01')
 
-Fetch_historical_data('b36407a99f24a168e534d190404b18f7','2018-02-01')
 
+Currency_conversion('access_key','BTC','USD',10)
 
-Currency_conversion('b36407a99f24a168e534d190404b18f7','BTC','USD',10)
-
-Fetch_data_InSpecific_TimeFrame('b36407a99f24a168e534d190404b18f7','2018-04-01','2018-04-30','BTC')
+Fetch_data_InSpecific_TimeFrame('access_key','2018-04-01','2018-04-30','BTC')
 
 
 
