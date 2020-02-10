04e50218e44567312b1ca8ba14f0b6c1af720f3a
AminaAbdallah <58793357+AminaAbdallah@users.noreply.github.com>
2020-02-10 12:12:33 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,13 +1,13 @@
 
 
-from CurrencyAPI.testpack import print2
+
 from CurrencyAPI.Fetch_historical_data import Fetch_historical_data
 from CurrencyAPI.currency_conversion import Currency_conversion
 from CurrencyAPI.Fetch_data_in_aSpecific_timeFrame_conversion import Fetch_data_InSpecific_TimeFrame
 
 
 
-print2()
+
 Fetch_historical_data('b36407a99f24a168e534d190404b18f7','2018-02-01')
 
 
